e7d6386a5de571fee95db7695413b809ddc03564
Harshith Ashvi <harshith.ashvi@gmail.com>
2025-06-21 11:58:11 +0530

    add complex class and single epicycles to draw the path

--- sketch.js
+++ sketch.js
@@ -1,56 +1,22 @@
 let x = [];
-let y = []; // signal
 let fourierX = [];
-let fourierY = []; // discret fourier tranform of that signal
 let font;
 let rawPoints = [];
-
-function preload() {
-  // Load any readable .ttf/.otf font; here we use SourceCodePro from CDN
-  font = loadFont(
-    "https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/font/SourceCodePro-Regular.otf"
-  );
-}
 
 let time = 0;
 let path = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // Convert "Happy"
-  let pointsHappy = font.textToPoints("Harshith", 100, 150, 80, {
-    sampleFactor: 0.2,
-    simplifyThreshold: 0,
-  });
 
-  // Convert "Birthday"
-  let pointsBirthday = font.textToPoints("Ashvi", 100, 250, 80, {
-    sampleFactor: 0.2,
-    simplifyThreshold: 0,
-  });
-
-  // Combine and normalize points
-  rawPoints = [...pointsHappy, ...pointsBirthday];
-  const centeredPoints = rawPoints.map((p) => ({
-    x: p.x - width / 2,
-    y: p.y - height / 2,
-  }));
-
-  for (let i = 0; i < centeredPoints.length; i++) {
-    x.push(centeredPoints[i].x);
-    y.push(centeredPoints[i].y);
+  const skip = 10;
+  for (let i = 0; i < drawing.length; i += skip) {
+    const c = new Complex(drawing[i].x, drawing[i].y);
+    x.push(c);
   }
-
-  // const skip = 10;
-  // for (let i = 0; i < drawing.length; i += skip) {
-  //   x.push(drawing[i].x);
-  //   y.push(drawing[i].y);
-  // }
   fourierX = dft(x);
-  fourierY = dft(y);
 
   fourierX.sort((a, b) => b.amp - a.amp);
-  fourierY.sort((a, b) => b.amp - a.amp);
 }
 
 function epiCycles(x, y, rotation, fourier) {
@@ -76,14 +42,9 @@ function epiCycles(x, y, rotation, fourier) {
 function draw() {
   background(0);
 
-  const vx = epiCycles(window.width / 2, 100, 0, fourierX);
-  const vy = epiCycles(100, height / 2, HALF_PI, fourierY);
-  const v = createVector(vx.x, vy.y);
+  const v = epiCycles(window.width / 2, height / 2, 0, fourierX);
 
   path.unshift(v);
-
-  line(vx.x, vx.y, v.x, v.y);
-  line(vy.x, vy.y, v.x, v.y);
 
   beginShape();
   noFill();
@@ -93,7 +54,7 @@ function draw() {
   }
   endShape();
 
-  const dt = TWO_PI / fourierY.length;
+  const dt = TWO_PI / fourierX.length;
   time += dt;
 
   if (time > TWO_PI) {
